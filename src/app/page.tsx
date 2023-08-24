d6b524e0af1cc5371e128ef5c7afd9fed15de539
Matias Seitour <matias.seitour01@gmail.com>
2023-08-23 22:16:55 -0300

    fix(page) change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { CategoriesHome } from "./components/CategoriesHome";
 import { SearchHome } from "./components/SearchHome";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full bg-slate-200 pt-24 px-2">
+    <main className="min-h-screen w-full bg-slate-100 pt-24 px-2">
       <SearchHome />
+      <CategoriesHome />
     </main>
   );
 }
