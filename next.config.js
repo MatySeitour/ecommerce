5dd0265b64be56c1139ee9cc2aeca10defcf27bf
Matias Seitour <matias.seitour01@gmail.com>
2023-09-03 10:45:15 -0300

    feat(next.config) add s3 domain and enable server actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "matungo.s3.us-east-2.amazonaws.com"
+        ],
+    },
+    experimental: {
+        serverActions: true,
+    },
+}
 
 module.exports = nextConfig
