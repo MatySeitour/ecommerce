cc708d650bbb8fa013b5dfa387a6b05729d48627
Matias Seitour <matias.seitour01@gmail.com>
2023-09-03 10:44:01 -0300

    feat(tailwind.config) add colors pallet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,60 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#a00",
+            foreground: "#666",
+            primary: {
+              DEFAULT: "#222",
+              foreground: "#a00",
+            },
+            secondary: {
+              DEFAULT: "#666",
+              foreground: "#a00",
+            },
+            details: {
+              low: "#60a5fa",
+              medium: "#3b82f6",
+              strong: "#1d4ed8",
+            },
+            hover: "#00A55A",
+          },
+        },
+        dark: {
+          colors: {
+            background: "#000",
+            foreground: "#000",
+            primary: {
+              DEFAULT: "#03a",
+              foreground: "#3a0",
+            },
+            focus: "#30a",
+            hover: "#035",
+          },
+        },
+      },
+    }),
+  ],
+};
+export default config;
