bdaddfcc9c065f593f23c9b49877a46979bbca36
Matias Seitour <matias.seitour01@gmail.com>
2023-09-03 10:51:26 -0300

    feat(categories) add categories component

--- src/app/components/CategoriesHome.tsx
+++ src/app/components/CategoriesHome.tsx
@@ -1,99 +1,37 @@
 import Image from "next/image";
-import cookies from "../../../public/cookies.png";
+import { CreateCategory } from "./CreateCategories";
 
-export const CategoriesHome = (): JSX.Element => {
+export const CategoriesHome = ({ categories }: { categories: any[] }) => {
   return (
-    <article className="h-auto w-full px-4 py-2">
-      <div className="flex justify-between items-center mb-4">
-        <p className="font-semibold">Categorias</p>
-        <p className="text-sm text-green-700 font-medium">Ver más</p>
-      </div>
-      <ul className="w-full h-32 flex gap-3 items-center justify-center overflow-x-scroll overflow-y-hidden flex-col flex-wrap">
-        <li className="w-auto min-w-[7rem] max-w-[8rem] h-full">
-          <div className="w-full h-full flex flex-col items-center">
-            <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
-              <Image
-                alt="cookies"
-                width={400}
-                height={400}
-                src={cookies}
-                className="w-24 object-cover"
-              />
-            </div>
-            <p>vegetables</p>
-          </div>
-        </li>
-        <li className="w-auto min-w-[7rem] max-w-[8rem] h-full">
-          <div className="w-full h-full flex flex-col items-center">
-            <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
-              <Image
-                alt="cookies"
-                width={400}
-                height={400}
-                src={cookies}
-                className="w-24 object-cover"
-              />
-            </div>
-            <p>vegetables</p>
-          </div>
-        </li>
-        <li className="w-auto min-w-[7rem] max-w-[8rem] h-full">
-          <div className="w-full h-full flex flex-col items-center">
-            <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
-              <Image
-                alt="cookies"
-                width={400}
-                height={400}
-                src={cookies}
-                className="w-24 object-cover"
-              />
-            </div>
-            <p>vegetables</p>
-          </div>
-        </li>
-        <li className="w-auto min-w-[7rem] max-w-[8rem] h-full">
-          <div className="w-full h-full flex flex-col items-center">
-            <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
-              <Image
-                alt="cookies"
-                width={400}
-                height={400}
-                src={cookies}
-                className="w-24 object-cover"
-              />
-            </div>
-            <p>vegetables</p>
-          </div>
-        </li>
-        <li className="w-auto min-w-[7rem] max-w-[8rem] h-full">
-          <div className="w-full h-full flex flex-col items-center">
-            <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
-              <Image
-                alt="cookies"
-                width={400}
-                height={400}
-                src={cookies}
-                className="w-24 object-cover"
-              />
-            </div>
-            <p>vegetables</p>
-          </div>
-        </li>
-        <li className="w-auto min-w-[7rem] max-w-[8rem] h-full">
-          <div className="w-full h-full flex flex-col items-center">
-            <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
-              <Image
-                alt="cookies"
-                width={400}
-                height={400}
-                src={cookies}
-                className="w-24 object-cover"
-              />
-            </div>
-            <p>vegetables</p>
-          </div>
-        </li>
-      </ul>
-    </article>
+    <section className="h-auto w-full px-4 py-2">
+      <article className="mb-10">
+        <div className="flex justify-between items-center mb-4">
+          <p className="font-semibold">Categorias</p>
+          <p className="text-sm text-green-700 font-medium">Ver más</p>
+        </div>
+        <ul className="w-full h-32 flex gap-3 items-center justify-center overflow-x-scroll overflow-y-hidden flex-col flex-wrap">
+          {categories.map((category) => (
+            <li
+              key={category.ID}
+              className="w-auto min-w-[7rem] max-w-[8rem] h-full"
+            >
+              <div className="w-full h-full flex flex-col items-center">
+                <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
+                  <Image
+                    alt={category.Name}
+                    width={400}
+                    height={400}
+                    src={`${category.ImageUrl}`}
+                    className="w-24 object-cover"
+                  />
+                </div>
+                <p>{category.Name}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </article>
+      <CreateCategory />
+    </section>
   );
 };
