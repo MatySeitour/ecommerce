8525ce5425e53c8238d38644e57f350a34d33c1e
Matias Seitour <matias.seitour01@gmail.com>
2023-09-13 22:36:46 -0300

    fix(loginWithOther) change order class

--- src/app/components/LoginWithOther.tsx
+++ src/app/components/LoginWithOther.tsx
@@ -4,40 +4,40 @@ import { BsFacebook } from "react-icons/bs";
 
 export default function LoginWithOther() {
   return (
-    <div className="flex w-full h-auto justify-center py-4 px-2 flex-col gap-4">
-      <div className="flex justify-center items-center gap-4">
-        <span className="w-6 h-[0.1rem] bg-primary"></span>
+    <div className="flex h-auto w-full flex-col justify-center gap-4 px-2 py-4">
+      <div className="flex items-center justify-center gap-4">
+        <span className="h-[0.1rem] w-6 bg-primary"></span>
         <p className="text-xs text-primary">O inicia sesión con</p>
-        <span className="w-6 h-[0.1rem] bg-primary"></span>
+        <span className="h-[0.1rem] w-6 bg-primary"></span>
       </div>
-      <div className="w-full h-auto flex flex-row gap-2 px-8 md:items-center">
+      <div className="flex h-auto w-full flex-row gap-2 px-8 md:items-center">
         <Button
           variant="flat"
           radius="sm"
-          className="px-2 hover:bg-secondary/30 md:max-w-[14rem] md:w-full"
+          className="px-2 hover:bg-secondary/30 md:w-full md:max-w-[14rem]"
         >
-          <AiOutlineGoogle className="w-5 h-5 text-primary" />
-          <p className="font-bold text-primary hidden md:inline-block">
+          <AiOutlineGoogle className="h-5 w-5 text-primary" />
+          <p className="hidden font-bold text-primary md:inline-block">
             Google
           </p>
         </Button>
         <Button
           variant="flat"
           radius="sm"
-          className="px-2 hover:bg-secondary/30 md:max-w-[14rem] md:w-full"
+          className="px-2 hover:bg-secondary/30 md:w-full md:max-w-[14rem]"
         >
-          <AiOutlineInstagram className="w-5 h-5 text-primary" />
-          <p className="font-bold text-primary hidden md:inline-block">
+          <AiOutlineInstagram className="h-5 w-5 text-primary" />
+          <p className="hidden font-bold text-primary md:inline-block">
             Instagram
           </p>
         </Button>
         <Button
           variant="flat"
           radius="sm"
-          className="px-2 hover:bg-secondary/30 md:max-w-[14rem] md:w-full"
+          className="px-2 hover:bg-secondary/30 md:w-full md:max-w-[14rem]"
         >
-          <BsFacebook className="w-5 h-5 text-primary" />
-          <p className="font-bold text-primary hidden md:inline-block">
+          <BsFacebook className="h-5 w-5 text-primary" />
+          <p className="hidden font-bold text-primary md:inline-block">
             Facebook
           </p>
         </Button>
