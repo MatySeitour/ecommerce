d8d3198ba83aeed6a8bfeecbd2b0a88416628fc0
Matias Seitour <matias.seitour01@gmail.com>
2023-10-09 17:44:42 -0300

    fix(categoriesHome) order tailwind classes

--- src/app/components/CategoriesHome.tsx
+++ src/app/components/CategoriesHome.tsx
@@ -5,18 +5,18 @@ export const CategoriesHome = ({ categories }: { categories: any[] }) => {
   return (
     <section className="h-auto w-full px-4 py-2">
       <article className="mb-10">
-        <div className="flex justify-between items-center mb-4">
+        <div className="mb-4 flex items-center justify-between">
           <p className="font-semibold">Categorias</p>
-          <p className="text-sm text-green-700 font-medium">Ver más</p>
+          <p className="text-sm font-medium text-green-700">Ver más</p>
         </div>
-        <ul className="w-full h-32 flex gap-3 items-center justify-center overflow-x-scroll overflow-y-hidden flex-col flex-wrap">
+        <ul className="flex h-32 w-full flex-col flex-wrap items-center justify-center gap-3 overflow-y-hidden overflow-x-scroll">
           {categories.map((category) => (
             <li
               key={category.ID}
-              className="w-auto min-w-[7rem] max-w-[8rem] h-full"
+              className="h-full w-auto min-w-[7rem] max-w-[8rem]"
             >
-              <div className="w-full h-full flex flex-col items-center">
-                <div className="h-full w-full bg-green-300/100 rounded-md flex items-center justify-center">
+              <div className="flex h-full w-full flex-col items-center">
+                <div className="flex h-full w-full items-center justify-center rounded-md bg-green-300/100">
                   <Image
                     alt={category.Name}
                     width={400}
