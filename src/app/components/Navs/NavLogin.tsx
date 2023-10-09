c42c11400b04df84cabe354184c9be3d86637cee
Matias Seitour <matias.seitour01@gmail.com>
2023-10-09 17:34:06 -0300

    fix(navLogin) order tailwind classes

--- src/app/components/Navs/NavLogin.tsx
+++ src/app/components/Navs/NavLogin.tsx
@@ -2,19 +2,19 @@ import ButtonNav from "./ButtonNav";
 
 export default function NavLogin() {
   return (
-    <header className="w-full h-14 bg-transparent fixed">
-      <nav className="w-full h-full p-2 flex items-center justify-between">
+    <header className="fixed h-12 w-full bg-transparent">
+      <nav className="flex h-full w-full items-center justify-between px-4">
         <div>LOGO</div>
-        <div className="flex-row flex gap-6">
+        <div className="flex flex-row gap-6">
           <ButtonNav
-            className="bg-transparent w-full px-4 h-10 text-details-medium hover:bg-details-medium/90 hover:text-white border-details-medium  font-medium tracking-wide"
+            className="h-10 w-full border-details-medium bg-transparent px-2 font-medium tracking-wide text-details-medium  hover:bg-details-medium/90 hover:text-white"
             variant={undefined}
             text="Registrarse"
           />
           <ButtonNav
             variant={undefined}
             text="Demo"
-            className="w-full px-4 h-10 bg-details-medium text-white hover:bg-details-medium/90 border-details-medium  font-medium tracking-wide"
+            className="h-10 w-full border-details-medium bg-details-medium px-2 font-medium tracking-wide  text-white hover:bg-details-medium/90"
           />
         </div>
       </nav>
