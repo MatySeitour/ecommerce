7b5a2601c8cd8e624caf41f80aacbc46ad8bd8de
Matias Seitour <matias.seitour01@gmail.com>
2023-10-16 00:07:07 -0300

    fix(signup) import dynamic signup client component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,20 @@
 import SignupClient from "../components/SignupClient";
 import "../globals.css";
+import LoadingData from "../loading";
 import { varela } from "../utils/fonts";
+import dynamic from "next/dynamic";
 
 export default function register() {
+  const ClientPage = dynamic(() => import("../components/SignupClient"), {
+    ssr: false,
+    loading: LoadingData,
+  });
+
   return (
     <main
       className={`min-w-screen flex min-h-screen flex-row items-center justify-center bg-slate-200 ${varela.className}`}
     >
-      <SignupClient />
+      <ClientPage />
     </main>
   );
 }
