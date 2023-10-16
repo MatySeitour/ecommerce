4c20c7193883846a0211292c22c97d56126a6ffb
Matias Seitour <matias.seitour01@gmail.com>
2023-10-16 00:08:41 -0300

    fix(signupClient) change background

--- src/app/components/SignupClient.tsx
+++ src/app/components/SignupClient.tsx
@@ -4,7 +4,7 @@ import StepsInterface from "./StepsInterface";
 export default function SignupClient() {
   return (
     <>
-      <article className="flex h-screen w-full items-center justify-center bg-slate-200 py-4">
+      <article className="flex h-screen w-full items-center justify-center bg-slate-300/20 py-4">
         <StepsInterface />
         <FormSignUp />
       </article>
