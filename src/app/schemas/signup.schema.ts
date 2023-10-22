88c94b2ca72f180a1b3b70d04ce040d008d9aad4
Matias Seitour <matias.seitour01@gmail.com>
2023-10-22 13:23:14 -0300

    feat(signupSchema) add more fields to second step

--- src/app/schemas/signup.schema.ts
+++ src/app/schemas/signup.schema.ts
@@ -38,29 +38,54 @@ export const firstStepSchema = z
     path: ["confirmPassword"],
   });
 
-export const secondStepSchema = z
-  .object({
-    logoCompany: z
-      .any()
-      .refine((file) => {
-        return file.length == 0 || ACCEPTED_IMAGE_TYPES.includes(file[0]?.type);
-      }, "Sólo se permiten imagenes con la extensión .png")
-      .optional(),
-    businessType: z.string().optional(),
-    // refine((select) => {
-    //   return (
-    //     select === "Tengo un negocio fisico" ||
-    //     select === "Tengo un negocio digital"
-    //   );
-    // }, "Selecciona una opción"),
-    businessAddress: z.string().optional(),
-    businessOpen: z.string().optional(),
-  })
-  .refine((example) => example.businessType !== "Tengo un negocio fisico", {
-    message: "Por favor, escribe una dirección",
-    path: ["businessOpen"],
-  })
-  .refine((data) => data.businessType !== "Tengo un negocio fisico", {
-    message: "Por favor, escribe una dirección",
-    path: ["businessAddress"],
-  });
+export const secondStepSchema = z.object({
+  logoCompany: z
+    .any()
+    .refine((file) => {
+      return file.length == 0 || ACCEPTED_IMAGE_TYPES.includes(file[0]?.type);
+    }, "Sólo se permiten imagenes con la extensión .png")
+    .optional(),
+
+  businessAddress: z.string().refine((province) => {
+    console.log(province.length);
+    return province.length != 0;
+  }, "Escribe la dirección de tu negocio."),
+
+  businessProvince: z.string().refine((province) => {
+    console.log(province.length);
+    return province.length != 0;
+  }, "Selecciona una provincia."),
+
+  businessCity: z.string().refine((city) => {
+    console.log(city.length);
+    return city.length != 0;
+  }, "Selecciona una ciudad."),
+
+  businessOpen: z.string().refine((business) => {
+    console.log(business.length);
+    return business.length != 0;
+  }, "Selecciona un dia."),
+
+  businessClosed: z.string().refine((business) => {
+    console.log(business.length);
+    return business.length != 0;
+  }, "Selecciona un dia."),
+
+  businessOpeningHours: z
+    .string()
+    .regex(
+      new RegExp(/^(0[1-9]|1[0-2]):[0-5][0-9]$/),
+      "Selecciona un horario.",
+    ),
+  // .refine((openHour) => {
+  //   console.log(openHour);
+  //   return openHour.length != 0;
+  // }, "Sólo se permiten imagenes con la extensión .png"),
+
+  businessClosedHours: z
+    .string()
+    .regex(
+      new RegExp(/^(0[1-9]|1[0-2]):[0-5][0-9]$/),
+      "Selecciona un horario.",
+    ),
+});
