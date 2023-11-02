3712acf1f821357ef429629fd9704540dae8864b
Matias Seitour <matias.seitour01@gmail.com>
2023-11-01 23:20:21 -0300

    feat(tailwind) add button style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "button-style": "linear-gradient(to top right, #60a5fa, #1d4ed8)",
       },
       keyframes: {
         loadingIcons: {
