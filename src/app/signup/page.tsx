2bf5d7df419f65eba2ce83515da2b2006b996206
Matias Seitour <matias.seitour01@gmail.com>
2023-11-26 23:11:48 -0300

    refactor(signup/page) change import name

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,25 @@
-import SignupClient from "../components/SignupClient";
 import "../globals.css";
 import LoadingData from "../loading";
 import { varela } from "../utils/fonts";
 import dynamic from "next/dynamic";
+import { cookies } from "next/headers";
+import { protectVerifyAccount } from "../functions/protectRoutes";
+import { DataUserAccount } from "../types";
 
-export default function register() {
+// async function getData() {
+//   const sessionCookie = cookies().get("authorization");
+//   const a = await protectVerifyAccount(sessionCookie);
+//   console.log(a);
+//   return a;
+// }
+
+export default async function register() {
   const ClientPage = dynamic(() => import("../components/SignupClient"), {
     ssr: false,
     loading: LoadingData,
   });
+
+  // const dataUser: DataUserAccount = await getData();
 
   return (
     <main
