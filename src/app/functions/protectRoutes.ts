33d7e1c0180be3699a8af6d82959e226fb6b0b7e
Matias Seitour <matias.seitour01@gmail.com>
2023-12-04 22:04:32 -0300

    feat(protectRoutes) add protectRouteAccountSettings and protectRouteSignUp

--- src/app/functions/protectRoutes.ts
+++ src/app/functions/protectRoutes.ts
@@ -2,14 +2,14 @@ import { redirect } from "next/navigation";
 import { getSession } from "./session";
 
 export default async function protectRoutes(sessionCookie: any) {
-  //   const sessionCookie = cookies().get("authorization");
-
   if (sessionCookie?.name != "authorization") return redirect("/login");
   const isLogin = await getSession(sessionCookie);
 
   if (!isLogin) return redirect("/signup");
   if (isLogin.isVerify === false) {
     return redirect("/verify-account");
+  } else if (isLogin.current_step != 4) {
+    return redirect("/account-settings");
   } else {
     return isLogin;
   }
@@ -24,4 +24,19 @@ export async function protectVerifyAccount(sessionCookie: any) {
   } else {
     return redirect("/");
   }
+}
+
+export async function protectRoutesAccountSettings(sessionCookie: any) {
+  if (sessionCookie?.name != "authorization") return redirect("/login");
+  const isLogin = await getSession(sessionCookie);
+
+  if (isLogin.isVerify === false) {
+    return redirect("/verify-account");
+  } else {
+    return isLogin;
+  }
+}
+
+export async function protectRoutesSignUp(sessionCookie: any) {
+  if (sessionCookie?.name == "authorization") return redirect("/");
 }
