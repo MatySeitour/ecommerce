090a46421d45ea6abb3c6d1a7de1b327a2371ea7
Matias Seitour <matias.seitour01@gmail.com>
2023-12-05 22:12:29 -0300

    feat(formSecondStep) add styles

--- src/app/components/formsSignup/FormSecondStep.tsx
+++ src/app/components/formsSignup/FormSecondStep.tsx
@@ -9,15 +9,18 @@ import SignupInput from "../inputs/SignupInput";
 import SelectInput from "../SelectsForm/SelectInput";
 import TimeInput from "../inputs/TimeInput";
 import { FaTrashAlt } from "react-icons/fa";
+import axios from "axios";
 
 export default function FormSecondStep({
   step,
   errors,
   register,
+  logoImage,
 }: {
   step: number;
   errors: any;
   register: any;
+  logoImage: string;
 }) {
   const dateBusinessOpen = [
     "Lunes",
@@ -31,18 +34,62 @@ export default function FormSecondStep({
 
   const [messageImageError, setMessageImageError] = useState<boolean>(false);
   const [selectBusiness, setSelectBusiness] = useState<string>("");
+  const [logoImageUrl, setLogoImageUrl] = useState<string>(logoImage);
 
   const { logoFile, setFileData } = useStep();
+
+  // async function deleteLogo() {
+  //   try {
+  //     const res = await axios.delete(
+  //       "http://localhost:3000/delete-logo",
+
+  //       { withCredentials: true },
+  //     );
+  //     console.log("esta es la respuesta", res);
+  //     // nextStep(step, data);
+  //     return true;
+  //   } catch (error: any) {
+  //     if (error?.response?.data == "Este mail no es válido") {
+  //       // setErrorEmail(error?.response?.status);
+  //       console.error(error?.response);
+  //       return false;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  // }
+  // async function saveLogoImage(logoCompany: any) {
+  //   try {
+  //     const res = await axios.postForm(
+  //       "http://localhost:3000/save-logo",
+  //       { logoCompany: logoCompany },
+  //       { withCredentials: true },
+  //     );
+  //     console.log("esta es la respuesta", res);
+  //     // nextStep(step, data);
+  //     return true;
+  //   } catch (error: any) {
+  //     if (error?.response?.data == "Este mail no es válido") {
+  //       // setErrorEmail(error?.response?.status);
+  //       console.error(error);
+  //       return false;
+  //     } else {
+  //       console.error(error);
+
+  //       return false;
+  //     }
+  //   }
+  // }
 
   return (
     <div
       className={
-        step === 2
+        step != 2
           ? `visible flex w-full translate-x-0 flex-col gap-4 transition-all sm:absolute sm:grid sm:grid-cols-2`
           : `invisible absolute grid translate-x-[40rem] grid-cols-2 gap-4 transition-all`
       }
     >
-      <div className="flex flex-col gap-2 sm:items-start sm:justify-start">
+      <div className="flex flex-col gap-0 sm:items-start sm:justify-start">
         <div className="hidden flex-row items-center justify-center gap-4 lg:flex">
           <label
             htmlFor="logoCompany"
@@ -77,6 +124,7 @@ export default function FormSecondStep({
                     setFileData(logoFile);
                   } else {
                     setFileData(e.target.files[0]);
+                    // saveLogoImage(e.target.files[0]);
                     setMessageImageError(false);
                   }
                 }
@@ -92,10 +140,13 @@ export default function FormSecondStep({
               {`Seleccionar logo (opcional)`}
             </label>
             <Button
-              onClick={() => setFileData("")}
+              onClick={() => {
+                setFileData("");
+                setLogoImageUrl("");
+              }}
               // isLoading={isSubmitting ? true : false}
-              isDisabled={logoFile ? false : true}
-              className="hidden h-10 w-10 rounded-full bg-error-medium text-[0.7rem] text-white hover:bg-error-medium/90 lg:inline-block lg:w-full lg:max-w-[8rem]"
+              isDisabled={logoFile ? false : logoImageUrl ? false : true}
+              className=" h-10 w-10 rounded-full bg-error-medium text-[0.7rem] text-white hover:bg-error-medium/90 lg:inline-block lg:w-full lg:max-w-[8rem]"
               radius="sm"
               size="sm"
               type="button"
@@ -104,15 +155,12 @@ export default function FormSecondStep({
             </Button>
             <Button
               onClick={() => setFileData("")}
-              // isLoading={isSubmitting ? true : false}
               isDisabled={logoFile ? false : true}
               className="inline-block h-10 w-10 min-w-0 rounded-full bg-error-medium p-0 text-[0.7rem] text-white hover:bg-error-medium/90 lg:hidden lg:w-full lg:max-w-[8rem]"
               radius="sm"
               size="sm"
               type="button"
-            >
-              a
-            </Button>
+            ></Button>
             <span
               className={`hidden text-[0.66rem] text-secondary lg:inline-block ${
                 messageImageError && `!text-error-medium`
@@ -133,9 +181,15 @@ export default function FormSecondStep({
                 htmlFor="logoCompany"
                 className="flex h-20 w-20 cursor-pointer flex-row items-center justify-center gap-2 overflow-hidden rounded-full border border-white/30 bg-white p-1 text-sm text-white outline-none placeholder:text-white focus:border-white lg:h-24 lg:w-24"
               >
-                {logoFile ? (
+                {logoImage != "" ? (
                   <Image
-                    src={`${URL.createObjectURL(logoFile)}`}
+                    src={`${
+                      logoFile != ""
+                        ? URL.createObjectURL(logoFile)
+                        : logoImageUrl != ""
+                        ? logoImage
+                        : "/cookies.png"
+                    }`}
                     alt="user"
                     width={500}
                     height={500}
@@ -179,8 +233,11 @@ export default function FormSecondStep({
               />
 
               <Button
-                onClick={() => setFileData("")}
-                isDisabled={logoFile ? false : true}
+                onClick={() => {
+                  setFileData("");
+                  setLogoImageUrl("");
+                }}
+                isDisabled={logoFile ? false : logoImageUrl ? false : true}
                 className="flex h-10 w-10 min-w-0 bg-error-medium p-0 text-[0.7rem] text-white hover:bg-error-medium/90 lg:hidden lg:w-full lg:max-w-[8rem]"
                 radius="sm"
                 size="sm"
@@ -198,11 +255,6 @@ export default function FormSecondStep({
             </span>
           </div>
         </div>
-        {/* {messageImageError && (
-          <span className="w-full text-center text-sm text-error-medium">
-            Solo se permite imagenes con formato png.
-          </span>
-        )} */}
       </div>
       <SelectInput
         step={step}
@@ -249,7 +301,7 @@ export default function FormSecondStep({
               aria-label="dia de apertura"
               classNames={{
                 trigger: [
-                  `min-h-4 max-h-8 w-full rounded-[4px] px-2 border ${
+                  `min-h-4 max-h-8 w-full rounded-[4px] px-2 border  border-details-low  ${
                     errors.openWeek &&
                     `border-red-500 bg-error-low text-error-medium`
                   }`,
@@ -288,7 +340,7 @@ export default function FormSecondStep({
               aria-label="dia de cierre"
               classNames={{
                 trigger: [
-                  `min-h-4 max-h-8 w-full rounded-[4px] px-2 border ${
+                  `min-h-4 max-h-8 w-full rounded-[4px] px-2 border  border-details-low ${
                     errors.closedWeek &&
                     `border-red-500 bg-error-low text-error-medium`
                   }`,
