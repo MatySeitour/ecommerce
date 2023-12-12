ec13ae5509a75aa8ef8549a4ab4f5bc50114f73c
Matias Seitour <matias.seitour01@gmail.com>
2023-12-12 00:04:57 -0300

    feat(signupClient) add onboarding on signup page

--- src/app/components/SignupClient.tsx
+++ src/app/components/SignupClient.tsx
@@ -1,12 +1,14 @@
-import FormSignUp from "../components/FormSignUp";
-import StepsInterface from "./StepsInterface";
+import Onboarding from "./Onboarding";
 
 export default function SignupClient() {
   return (
     <>
-      <article className="flex h-screen w-full items-center justify-center bg-slate-300/20 py-4">
+      {/* <article className="flex h-screen w-full items-center justify-center bg-slate-300/20 py-4">
         <StepsInterface />
         <FormSignUp />
+      </article> */}
+      <article className="flex h-screen w-full flex-col items-center justify-center bg-button-style py-4">
+        <Onboarding />
       </article>
     </>
   );
