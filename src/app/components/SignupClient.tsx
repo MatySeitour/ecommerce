0e2752ca0eba6450b742252108cc35c1add61336
Matias Seitour <matias.seitour01@gmail.com>
2023-12-14 23:13:14 -0300

    feat(signupClient) change background

--- src/app/components/SignupClient.tsx
+++ src/app/components/SignupClient.tsx
@@ -1,4 +1,5 @@
 import Onboarding from "./Onboarding";
+import EffectBackground from "./Onboarding/EffectBackground";
 
 export default function SignupClient() {
   return (
@@ -7,7 +8,12 @@ export default function SignupClient() {
         <StepsInterface />
         <FormSignUp />
       </article> */}
-      <article className="flex h-screen w-full flex-col items-center justify-center bg-button-style py-4">
+
+      <article className="relative z-10 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-button-style py-4">
+        <EffectBackground className="hidden after:left-0 after:top-0 after:-scale-x-100 md:inline-block" />
+        <EffectBackground className="after:right-1/2 after:top-40 after:-translate-y-8 after:translate-x-1/2" />
+        <EffectBackground className="after:-bottom-28 after:right-0 after:-translate-y-8 after:translate-x-1/2" />
+
         <Onboarding />
       </article>
     </>
