9913773cfafb8a51931ace78552ca1767d908579
Matias Seitour <matias.seitour01@gmail.com>
2023-12-14 23:12:51 -0300

    feat(fonts) add new font weight

--- src/app/utils/fonts.tsx
+++ src/app/utils/fonts.tsx
@@ -1,6 +1,6 @@
 import { Lexend } from "next/font/google";
 
 export const varela = Lexend({
-  weight: ["400", "600", "800"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   subsets: ["latin"],
 });
